e12cf8599403a264d151028dc11567bee4e8291f
Kay Ip <kayip@powa.com>
2016-01-06 16:45:55 +0800

    update existing lesson01 to lesson03, add lesson01

--- lesson01/lesson01.java
+++ lesson01/lesson01.java
@@ -1,68 +1,28 @@
 import java.util.*;
-import java.util.stream.IntStream;
 
 public class Main {
 	public static void main(String[] args) {
-		int result01 = tapeEquilibrium(new int[]{3, 1, 2, 4, 3});
-		int result = PermMissing(new int[]{1, 3, 5, 2});
-		int jump = frogJump(10, 85, 30);
+		int q1 = solution(20);
+        System.out.println(q1);
 	}
 
-	// question 1, Correctness 100%, performance: 0%
-    public static int tapeEquilibrium(int A[]) {
-        int leftSum = 0;
-        int rightSum = 0;
+	// question 1, Correctness 100%, performance: 100%
+    public static int binaryGap(int N) {
+        String binaryStr = Integer.toBinaryString(N);
+        char[] charArray = binaryStr.toCharArray();
 
-        int minDiff = 999999999;
-        for (int i = 1; i < A.length; i++) {
-            int[] leftArray = Arrays.copyOfRange(A, 0, i);
-            int[] rightArray = Arrays.copyOfRange(A, i, A.length);
-            int sumLeft = IntStream.of(leftArray).sum();
-            int sumRight = IntStream.of(rightArray).sum();
-            System.out.println("sum l: " + sumLeft + " sumR: " + sumRight);
-            if (Math.abs(sumRight - sumLeft) < minDiff) {
-                minDiff = Math.abs(sumRight - sumLeft);
+        SortedSet<Integer> gapSet = new TreeSet<Integer>();
+        int gap = 0;
+        for (char a : charArray) {
+            if (a=='1') {
+                gapSet.add(gap);
+                gap = 0;
+            } else {
+                gap = gap+1;
             }
         }
-        System.out.println("minDiff: " + minDiff);
-        return minDiff;
+
+        return gapSet.last();
     }
 
-	// question 02, Correctness 100%, performance: 100%
-    public static int permMissing(int A[]) {
-        if (A.length == 0) {
-            return 1;
-        }
-
-        Arrays.sort(A);
-        int lastElement = A[A.length - 1];
-        int startElement = A[0];
-        if (startElement != 1) {
-            return 1;
-        }
-
-        int sumOfArray = 0;
-        int sumTarget = 0;
-        for (int n : A) {
-            sumOfArray += n;
-        }
-
-        for (int i = 0; i < A.length+1; i++) {
-            sumTarget = sumTarget + startElement + i;
-        }
-
-        return sumTarget - sumOfArray;
-    }
-
-	// question 03, Correctness 100%, performance: 100%
-	public static int frogJump(int x, int y, int d) {
-        int jump = (y - x) / d;
-        int remain = (y - x) % d;
-
-        if (remain > 0) {
-            jump = jump + 1;
-        }
-        return jump;
-    }
-	
 }
